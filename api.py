f7a176a2a238cd8ce0dabfb2d09faf3d3540de03
yohichi <s2210653@u.tsukuba.ac.jp>
2024-03-31 10:55:27 +0900

    Add using api script

---
+++ api.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import requests
+import json
+
+def get_address(zipcode):
+    r = requests.get(" https://zipcloud.ibsnet.co.jp/api/search?zipcode={}".format(zipcode))
+    j = json.loads(r.text)
+    if j["status"] != 200:
+        return "error"
+    else:
+        return j["results"][0]["address1"] + j["results"][0]["address2"] + j["results"][0]["address3"]
+
+if __name__ == "__main__":
+    print(get_address(input()))
