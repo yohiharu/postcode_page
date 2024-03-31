fbbcc90a52e0cf63c9f766081480c15fb98c7087
yohichi <s2210653@u.tsukuba.ac.jp>
2024-03-31 12:10:44 +0900

    Add db.py

---
+++ flaskr/db.py
@@ -0,0 +1,6 @@
+import sqlite3
+
+def create_table(name):
+    con = sqlite3.connect(name)
+    con.execute("CREATE TABLE IF NOT EXISTS codes (code)")
+    con.close()
--- flaskr/main.py
+++ flaskr/main.py
@@ -2,8 +2,12 @@
 
 from flask import Flask
 from flask import render_template, request
+import sqlite3
+
 import api
 import db
+
+DATABASE = "database.db"
 
 app = Flask(__name__)
 
@@ -22,11 +26,15 @@ def form():
             "error.html"
         )
     else:
+        con = sqlite3.connect(DATABASE)
+        con.execute("INSERT INTO codes VALUES(?)", [code])
+        con.commmit()
+        con.close()
         return render_template(
             "result.html",
             address = address
         )
 
 if __name__ == "__main__":
-    db.create_table()
+    db.create_table(DATABASE)
     app.run(host="0.0.0.0", port=443)
