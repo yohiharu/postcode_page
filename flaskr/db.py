9f45d8a070fb68295f826702540ff7d645a1d80c
yohichi <s2210653@u.tsukuba.ac.jp>
2024-04-02 01:56:08 +0900

    Make db.py a better code

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,5 +4,5 @@ import sqlite3
 
 def create_table(name):
     con = sqlite3.connect(name)
-    con.execute("CREATE TABLE IF NOT EXISTS codes (id integer primary key autoincrement, code)")
+    con.execute("CREATE TABLE IF NOT EXISTS codes (id integer primary key autoincrement, code int)")
     con.close()
