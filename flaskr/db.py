d47a9c6610c3238666ec75e330431081a59b9a59
yohiharu <s2210653@s.tsukuba.ac.jp>
2024-04-04 23:41:30 +0900

    Add change

--- flaskr/db.py
+++ flaskr/db.py
@@ -8,6 +8,6 @@ def create_table(name):
     con.execute("CREATE TABLE IF NOT EXISTS codes (id integer primary key autoincrement, code int)")
     con.close()
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     name = "database.db"
     create_table(name)
